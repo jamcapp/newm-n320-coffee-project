7d156f1c0cb774a111020219a7ded61d4d75a0ab
mmarckwo <78805766+mmarckwo@users.noreply.github.com>
2021-11-17 23:49:41 -0500

    Another component split
    even more organization

--- src/Components/CoffeeSelect.js
+++ src/Components/CoffeeSelect.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react"; 
+import CoffeeSelectIcons from "./CoffeeSelectIcons";
 import "../styles.css"; 
 
 export default function CoffeeSelect(props) {
     const [testVal, setTestVal] = useState(0);
 
     return(
-        <div class="CoffeeSelect">coffee select
-            <div class="CoffeeIcon">one</div>
-            <div class="CoffeeIcon">two</div>
-            <div class="CoffeeIcon">three</div>
-            <div class="CoffeeIcon">four</div>
-            <div class="CoffeeIcon">five</div>
+        <div class="CoffeeSelect">Select Coffee
+            <CoffeeSelectIcons />
         </div>
     )
 }
---
+++ src/Components/CoffeeSelectIcons.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react"; 
+import "../styles.css"; 
+
+export default function CoffeeSelect(props) {
+    const [testVal, setTestVal] = useState(0);
+
+    return(
+        <div>
+            <div class="CoffeeIcon">one</div>
+            <div class="CoffeeIcon">two</div>
+            <div class="CoffeeIcon">three</div>
+            <div class="CoffeeIcon">four</div>
+            <div class="CoffeeIcon">five</div>
+        </div>
+    )
+}
