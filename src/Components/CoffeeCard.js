b0d374cb1423ce63bad34560f410b9b7b06367e3
mmarckwo <78805766+mmarckwo@users.noreply.github.com>
2021-11-17 21:21:15 -0500

    Project setup
    Changed App to a class, created two components.

---
+++ src/Components/CoffeeCard.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react"; 
+
+export default function CoffeeCard(props) {
+    const [testVal, setTestVal] = useState(0);
+
+    return(
+        <div>coffee card</div>
+    )
+}
---
+++ src/Components/CoffeeSelect.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react"; 
+
+export default function CoffeeSelect(props) {
+    const [testVal, setTestVal] = useState(0);
+
+    return(
+        <div>coffee select</div>
+    )
+}
