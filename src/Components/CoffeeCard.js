c04c21b6ca5b35b02d9cb8114099f24ec58c3f1d
mmarckwo <78805766+mmarckwo@users.noreply.github.com>
2021-11-17 21:45:41 -0500

    Progress update
    more stuff

--- src/Components/CoffeeCard.js
+++ src/Components/CoffeeCard.js
@@ -5,9 +5,13 @@ export default function CoffeeCard(props) {
     const [testVal, setTestVal] = useState(0);
 
     return(
-        <div className="CoffeeCard">coffee card
+        <div className="CoffeeCard">
             <div className="CoffeeName">Coffee Name
                 <div className="CoffeeSubtext">coffee subtext</div>
+            </div>
+            <div className="InfoBoxes">
+                <div className="InfoBox one">hello1</div>
+                <div className="InfoBox two">hello2</div>
             </div>
         </div>
     )
