4e61beb284b2fc3d1b2985ec72c076c9334aad6e
mmarckwo <78805766+mmarckwo@users.noreply.github.com>
2021-12-05 20:13:30 -0500

    card animation update
    card slides out when debug button is clicked

--- src/Components/CoffeeCard.js
+++ src/Components/CoffeeCard.js
@@ -5,33 +5,43 @@ import { CSSTransition } from "react-transition-group";
 
 export default function CoffeeCard(props) {
   const [testVal, setTestVal] = useState(0);
+  const [cardState, setCardState] = useState(true);
+  const [animationLength, setAnimationLength] = useState(1250);
 
-  // use effect transition
+  // on effect, set cardstate back to true after slide animation is complete.
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCardState(true)
+    }, animationLength);
+  })
 
   return (
-    <div className="CoffeeCard">
-      {/* IMAGE AND DESCRIPTION */}
-      <div className="LeftSide">
-        <div className="CoffeeImage">
-          <img src={props.bean.image} />
-        </div>
-        <div className="CoffeeDescription">
-          Description: {props.bean.description}
-        </div>
-      </div>
-
-      <div className="RightSide">
-        {/* CARD HEADER */}
-        <div className="CardHeader">
-          <div className="CoffeeName">
-            {props.bean.name}
-            <div className="CoffeeSubtext">{props.bean.subText}</div>
+    <CSSTransition key={testVal} in={cardState} timeout={animationLength} classNames="Coffee-Slide">
+      <div className="CoffeeCard">
+        {/* IMAGE AND DESCRIPTION */}
+        <div className="LeftSide">
+          <div className="CoffeeImage">
+            <img src={props.bean.image} />
+          </div>
+          <div className="CoffeeDescription">
+            Description: {props.bean.description}
           </div>
         </div>
 
-        {/* INFORMATION BOXES */}
-        <CoffeeCardInfoBoxes bean={props.bean} />
+        <div className="RightSide">
+          {/* CARD HEADER */}
+          <div className="CardHeader">
+            <div className="CoffeeName">
+              {props.bean.name}
+              <div className="CoffeeSubtext">{props.bean.subText}</div>
+            </div>
+          </div>
+
+          {/* INFORMATION BOXES */}
+          <CoffeeCardInfoBoxes bean={props.bean} />
+        </div>
+        <button onClick={() => {setCardState(false); console.log(cardState);}}>[DEBUG] coffee state false</button>
       </div>
-    </div>
+    </CSSTransition>
   );
 }
